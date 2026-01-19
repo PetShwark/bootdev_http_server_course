378aec5dc61c67785f166834249a4383f946b4f4
Peter Schwenk <paschwenk@gmail.com>
2026-01-19 16:48:51 -0500

    After "Custom Handlers" lesson

---
+++ src/handlers/get_healthz.ts
@@ -0,0 +1,6 @@
+import { Response, Request } from "express";
+
+export async function handlerReadiness(req: Request, res: Response): Promise<void> {
+    res.set("Content-Type", "text/plain; charset=utf-8");
+    res.send("OK");
+}
