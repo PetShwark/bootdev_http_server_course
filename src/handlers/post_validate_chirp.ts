0dd989a253d2665874ac207f7e5d7d8b575fc6ca
Peter Schwenk <paschwenk@gmail.com>
2026-01-21 22:00:14 -0500

    After "The Profane" lesson

--- src/handlers/post_validate_chirp.ts
+++ src/handlers/post_validate_chirp.ts
@@ -4,18 +4,27 @@ interface RequestData {
     body: string;
 }
 
+const badWords = ["kerfuffle", "sharbert", "fornax"];
+
 function isRequestData(data: unknown): data is RequestData {
     if (typeof data !== 'object' || data === null) return false;
     const obj = data as Record<string, unknown>;
     return typeof obj.body === 'string';
 }
 
+function stripBadWords(input: string, badWordsList: string[]): string {
+    const words = input.split(" ").map((word) => {
+        return badWordsList.includes(word.toLowerCase()) ? "****" : word;
+    });
+    return words.join(" ");
+}
+
 const maxChirpLength = 140
 
 export async function handlerValidateChirp(req: Request, res: Response) {
     if (isRequestData(req.body)) {
-        if (req.body.body.length <= 140) {
-            res.status(200).send(JSON.stringify({ "valid": true }));
+        if (req.body.body.length <= maxChirpLength) {
+            res.status(200).send(JSON.stringify({ "cleanedBody": stripBadWords(req.body.body, badWords) }));
         } else {
             res.status(400).send(JSON.stringify({ "error": "Chirp is too long" }));
         }
