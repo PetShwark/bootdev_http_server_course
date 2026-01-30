8ca5a219393adab514e253ff764a8b4288cbbb15
Peter Schwenk <paschwenk@gmail.com>
2026-01-29 19:44:13 -0500

    After "JWTs" lesson

---
+++ src/lib/auth/auth.test.ts
@@ -0,0 +1,59 @@
+import { describe, it, expect, beforeAll } from "vitest";
+import { makeJWT, validateJWT, hashPassword, checkPasswordHash } from "./auth.js";
+
+describe("Password Hashing", () => {
+    const password1 = "correctPassword123!";
+    const password2 = "anotherPassword456!";
+    let hash1: string;
+    let hash2: string;
+
+    beforeAll(async () => {
+        hash1 = await hashPassword(password1);
+        hash2 = await hashPassword(password2);
+    });
+
+    it("should return true for the correct password1", async () => {
+        const result = await checkPasswordHash(password1, hash1);
+        expect(result).toBe(true);
+    });
+
+    it("should return true for the correct password2", async () => {
+        const result = await checkPasswordHash(password2, hash2);
+        expect(result).toBe(true);
+    });
+});
+
+describe("MakeJWT Testing", () => {
+    const secret1 = "correctPassword123!";
+    const userId1 = "test@example.com";
+    const secret2 = "anotherPassword456!";
+    const userId2 = "test2@example.com";
+    let jwt1: string;
+    let jwt2: string;
+
+    beforeAll(async () => {
+        jwt1 = makeJWT(userId1, 60, secret1);
+        jwt2 = makeJWT(userId2, 60, secret2);
+    });
+
+    it("should return userId1 for the correct secret1", async () => {
+        const result = validateJWT(jwt1, secret1);
+        expect(result).eq(userId1);
+    });
+
+    it("should return userId2 for the correct secret2", async () => {
+        const result = validateJWT(jwt2, secret2);
+        expect(result).eq(userId2);
+    });
+
+    it("should throw an error for incorrect secret", async () => {
+        expect(() => validateJWT(jwt1, "wrongSecret")).toThrow();
+    });
+
+    it("should throw an error for expired token", async () => {
+        const shortLivedJWT = makeJWT(userId1, 1, secret1);
+        // Wait for 2 seconds to ensure the token expires
+        await new Promise((resolve) => setTimeout(resolve, 2000));
+        expect(() => validateJWT(shortLivedJWT, secret1)).toThrow();
+    });
+});
--- src/lib/auth/auth.ts
+++ src/lib/auth/auth.ts
@@ -1,4 +1,7 @@
 import { hash, verify } from "argon2";
+import jwt from "jsonwebtoken";
+
+export type payload = Pick<jwt.JwtPayload, "iss" | "sub" | "iat" | "exp">;
 
 export async function hashPassword(password: string): Promise<string> {
     return await hash(password);
@@ -6,4 +9,28 @@ export async function hashPassword(password: string): Promise<string> {
 
 export async function checkPasswordHash(password: string, hash: string): Promise<boolean> {
     return await verify(hash, password);
+}
+
+export function makeJWT(userID: string, expiresIn: number, secret: string): string {
+    const payloadIat = Math.floor(Date.now() / 1000);
+    const payloadExp = payloadIat + expiresIn;
+    const payLoad: payload = {
+        iss: "chirpy",
+        sub: userID,
+        iat: payloadIat,
+        exp: payloadExp
+    }
+    return jwt.sign(payLoad, secret);
+}
+
+export function validateJWT(tokenString: string, secret: string): string {
+    type testPayload = jwt.JwtPayload;
+    const verifyOutput = jwt.verify(tokenString, secret);
+    if (typeof verifyOutput === "string") {
+        return verifyOutput;
+    } else if (verifyOutput.sub === undefined) {
+        throw new Error("JWT validation error.");
+    } else {
+        return verifyOutput.sub;
+    }
 }
